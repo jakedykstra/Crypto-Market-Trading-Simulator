df37c50122430571c02ed2d247180fb0961c6ee8
Riley Carroll <rcarroll98@gmail.com>
2018-09-21 23:49:08 -0700

    wip

---
+++ BlockTrade/src/BlockTrade.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import './src/amountinput.js';
+import User from './src/UserData';
+
+class BlockTrade extends React.Component {
+  render() {
+    return(
+    <User />
+    )
+  }
+};
--- BlockTrade/src/BlockTrade.test.js
+++ BlockTrade/src/BlockTrade.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import BlockTrade from './BlockTrade';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<BlockTrade />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
